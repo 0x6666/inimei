625fecd9a293306ab7bb0d73cf425d252f1f87fa
BorrowedStory <yangsongfwd@163.com>
2015-08-23 17:07:58 +0800

    adjust choose picture btn style and avatar preview

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,14 +1,31 @@
 module UsersHelper
 
-  def user_avatar(user, options = {size: 80})
-    img_url = user.avatar.url if user.avatar?
+  DEFAULT_USER_AVATAR = 'http://7xktkg.com1.z0.glb.clouddn.com/default_avatar.jpg'
+
+  def user_avatar_url(user, options = {size: 80})
+
+    img_url = user.avatar.url if (user && user.avatar?)
     size = options[:size]
+
     if img_url.blank?
-      img_url = "http://7xktkg.com1.z0.glb.clouddn.com/default_avatar.jpg?imageView/1/w/#{size}/h/#{size}"
+      img_url = "#{DEFAULT_USER_AVATAR}?imageView/1/w/#{size}/h/#{size}"
     else
       img_url += "?imageView/1/w/#{size}/h/#{size}"
     end
-    image_tag(img_url, alt: user.name, class: 'avatar')
+
+    img_url
+
+  end
+
+  def user_avatar(user, options = {size: 80})
+
+    img_url = user_avatar_url(user, options)
+
+    options[:alt] = user.name
+    options[:class] = 'avatar'
+    options[:size] = options[:size].to_s
+
+    image_tag(img_url, options)
   end
 
 end
