6bddc0889372cf0bc26336acfce64810c82ec0d4
BorrowedStory <yangsongfwd@163.com>
2015-09-06 21:09:29 +0800

    adjust schedules view

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -3,11 +3,8 @@ class Schedule < ActiveRecord::Base
   belongs_to :user
   default_scope -> { order(planed_completed_at: :desc) }
 
-  #validates
   validates :user_id, presence: true
-  validates :title, length: {maximum: 50}
-  validates :title, presence: true, if: :require_title_presence?
-  validates :content, presence: true, if: :require_content_presence?
+  validates :title, presence: true, length: {maximum: 50}
 
   def complete
     update_columns(completed: true, completed_at: Time.zone.now)
@@ -15,15 +12,6 @@ class Schedule < ActiveRecord::Base
 
   def uncomplete
     update_columns(completed: false, completed_at: nil)
-  end
-
-  private
-  def require_title_presence?
-    !self.content?
-  end
-
-  def require_content_presence?
-    !self.title?
   end
 
 end
